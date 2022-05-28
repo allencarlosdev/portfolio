0657d7c5ce0fe8c0e1758b12676978227677acb5
Carlos Allen <allencarlosDev@gmail.com>
2022-05-27 23:38:36 -0400

    Adding the layout component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Layout />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Layout from './components/Layout.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Layout
   }
 }
 </script>
@@ -20,7 +19,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fbd758;
 }
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>here the component layout</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Layout'
+    }
+</script>
+
+<style>
+
+</style>
