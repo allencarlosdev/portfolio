7381d788da55c54f3914ede76ed79303db37dd57
Carlos Allen <allencarlosDev@gmail.com>
2022-05-30 18:11:35 -0400

    Designing the footer

--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -1,15 +1,84 @@
 <template>
-    <div>
-        footer
-    </div>
+    <footer class="footer">
+        <div class="download-cv">
+            <a class="button-cv"><i class="fa-solid fa-circle-down"></i>Download CV</a>
+        </div>
+        <div class="contact-email">
+            <p class="contact-email__p"><i class="fa-solid fa-envelope"></i> allencarlosdev@gmail.com </p>
+        </div>
+
+        <div class="button-home">
+            <a class="button-cv"><i class="fa-solid fa-house-chimney"></i>Home</a>
+        </div>
+
+    </footer>
 </template>
 
 <script>
     export default {
-        
+        name:'FooterView',
+
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+    .footer {
+        background: var(--background-footer);
+        position: absolute;
+        bottom: 0;
+        height: 80px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    
+
+    .download-cv {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 120px;
+        height: 40px;
+        background: var(--letter-color);
+        margin-left: 100px;
+        cursor: pointer;
+        border-radius: 10px;
+    }
+
+    .download-cv:hover{
+        transition: .5s;
+        box-shadow: 0 0 5px var(--letter-color), 0 0 10px var(--letter-color), 0 0 20px var(--letter-color);
+    }
+
+    .button-cv {
+        display: flex;
+        color: var(--background-body);
+        transition: .4s ease-in-out all;
+        left: 40%;
+        
+    }
+
+    .fa-circle-down, .fa-house-chimney {
+        margin-right: 5px;
+        color: var(--background-body);
+    }
+
+    .button-home{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 120px;
+        height: 40px;
+        background: var(--letter-color);
+        cursor: pointer;
+        border-radius: 10px;
+        margin-right: 100px;
+    }
+
+    .button-home:hover {
+        transition: .5s;
+        box-shadow: 0 0 5px var(--letter-color), 0 0 10px var(--letter-color), 0 0 20px var(--letter-color);
+    }
 
 </style>
