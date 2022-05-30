afe9ed80b9000306ac3e4a68e2ac4dfcab15a89a
Carlos Allen <allencarlosDev@gmail.com>
2022-05-30 19:51:14 -0400

    Visually disabling home and portfolio navigation

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="nav">
-        <router-link to="/" class="nav__link">Home</router-link> |
+        <router-link to="/" class="nav__link">Home</router-link> 
         <router-link to="/portfolio" class="nav__link">About</router-link>
     </nav>
     <router-view/>
@@ -13,19 +13,9 @@
 </script>
 
 <style scoped>
-    .nav {
-        display: flex;
-        justify-content: center;
-        position: relative;
-    }
 
     .nav__link {
-        font-weight: bold;
-        color: blue;
-    }
-
-    .nav__link.router-link-exact-active {
-        color: #ff0000;
+        display: none;
     }
 
 </style>
