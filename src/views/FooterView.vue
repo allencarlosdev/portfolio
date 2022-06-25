6a5d5c99a9dfb573e18cf774f1798a820d72effe
Carlos Allen <allencarlosDev@gmail.com>
2022-06-24 21:12:49 -0400

    Adding new footer

--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -1,15 +1,15 @@
 <template>
     <footer class="footer">
-        <div class="download-cv">
-            <a rel="noopener noreferrer" href="https://drive.google.com/uc?export=download&id=1qRMUFfXmoLZ__oNNcQI5rZDIBNUW5Fue" download="Carlos-Allen-CV" class="nav__link"><i class="fa-solid fa-circle-down"></i>Download CV</a>
-        </div>
-        <div class="contact-email">
-            <p class="contact-email__p"><i class="fa-solid fa-envelope"></i> contact@allencarlosdev.com </p>
+        <button class="footer-btn">
+            <a rel="noopener noreferrer" href="https://drive.google.com/uc?export=download&id=1qRMUFfXmoLZ__oNNcQI5rZDIBNUW5Fue" download="Carlos-Allen-CV" class="footer__link"><i class="fa-solid fa-circle-down"></i>Download CV</a>
+        </button>
+        <div class="footer-email">
+            <p class="footer-email__p"><i class="fa-solid fa-envelope"></i> contact@allencarlosdev.com </p>
         </div>
 
-        <div class="button-home">
-            <router-link to="/" class="nav__link"><i class="fa-solid fa-house-chimney"></i>Home</router-link>
-        </div>
+        <button class="footer-btn">
+            <router-link to="/" class="footer__link"><i class="fa-solid fa-house-chimney"></i>Home</router-link>
+        </button>
 
     </footer>
 </template>
@@ -22,96 +22,46 @@
 
 <style scoped>
     .footer {
-        background: var(--background-body);
+        background: var(--background-footer);
         position: fixed;
-        z-index: var(--z-index90);
+        z-index: 4;
         bottom: 0;
         height: 5rem;
         width: 100%;
         display: flex;
         align-items: center;
         justify-content: space-between;
+        font-size: 1.5rem;
     }
     
+    .footer-btn {
+        margin: 5rem;
+        cursor: pointer;
+        border-radius: 0.625rem;
+        border: 0.25rem solid var(--letter-color);
+        border-radius: 1rem;
+    }
 
-    .download-cv {
+    .footer-btn:hover{
+        transition: .5s;
+        background:var(--letter-color);
+    }
+
+    .footer__link {
         display: flex;
         align-items: center;
         justify-content: center;
-        width: 7.5rem;
-        height: 2.5rem;
-        background: var(--letter-color);
-        margin-left: 6.25rem;
-        cursor: pointer;
-        border-radius: 0.625rem;
+        width: 12rem;
+        height: 3rem;
+        color: var(--letter-color);      
     }
 
-    .download-cv:hover{
-        transition: .5s;
-        background:linear-gradient(var(--letter-color), var(--letter-gradient));
-    }
-
-    .nav__link {
-        color: var(--background-body);      
+    .footer__link:hover {
+        color: var(--background-footer);      
     }
 
     .fa-circle-down, .fa-house-chimney {
-        margin-right: 0.313rem;
-        color: var(--background-body);
+        margin-right: 0.5rem;
     }
 
-    .button-home{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 7.5rem;
-        height: 2.5rem;
-        background: var(--letter-color);
-        cursor: pointer;
-        border-radius: 0.625rem;
-        margin-right: 6.25rem;
-    }
-
-    .button-home:hover {
-        transition: .5s;
-        background:linear-gradient(var(--letter-color), var(--letter-gradient));
-    }
-
-    @media screen and (max-width:630px){
-        .download-cv{
-            margin-left: 0.625rem;
-        }
-
-        .button-home {
-            margin-right: 0.625rem;
-        }
-    }
-
-    @media screen and (max-width:450px){
-        .download-cv{
-            width: 6.25rem;
-            margin-left: 0.313rem;
-        }
-        
-        .button-home {
-            width: 6.25rem;
-            margin-right: 0.313rem;
-        }
-    }
-
-    @media screen and (max-width:390px){
-        .download-cv{
-            width: 5rem;
-            margin-left: 0.313rem;
-        }
-        
-        .button-home {
-            width: 5rem;
-            margin-right: 0.313rem;
-        }
-
-        .fa-solid {
-            display: none;
-        }
-    }
 </style>
